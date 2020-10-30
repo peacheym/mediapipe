c57a7d19dd919a33723b1060046ebf44ac7213ca
Matt Peachey <peacheym@dal.ca>
2020-10-30 09:30:14 -0300

    Add mapper files, begin work on creating devices in calculator

--- mediapipe/calculators/libmapper_output/libmapper_output.cc
+++ mediapipe/calculators/libmapper_output/libmapper_output.cc
@@ -1,6 +1,8 @@
 #include "mediapipe/framework/calculator_framework.h"
 #include "mediapipe/framework/formats/landmark.pb.h"
 
+#include "mapper/mapper_cpp.h"
+
 namespace mediapipe
 {
 
@@ -23,6 +25,10 @@ namespace mediapipe
         ::mediapipe::Status Open(CalculatorContext *cc)
         {
 
+            // Add a new libmapper device -> Should this be a global?
+
+            mapper::Device dev("test");
+
             LOG(INFO) << "HELLO, WORLD!";
 
             return ::mediapipe::OkStatus();
@@ -40,7 +46,7 @@ namespace mediapipe
             const auto &input_landmarks = input_packet.Get<NormalizedLandmarkList>();
 
             LOG(INFO) << "Landmark 6: ("
-                      << input_landmarks.landmark(6).x() << "," << input_landmarks.landmark(6).y() << "," << input_landmarks.landmark(6       ).z();
+                      << input_landmarks.landmark(6).x() << "," << input_landmarks.landmark(6).y() << "," << input_landmarks.landmark(6).z();
 
             // for (int i = 0; i < input_landmarks.landmark_size(); ++i)
             // {
